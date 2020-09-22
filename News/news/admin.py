2a15333dd6ac631dcff9130effe23c7fec1cf681
Grigorev Grigorii <55593191+GrigorevGrigorii@users.noreply.github.com>
2020-09-22 20:55:21 +0300

    Update admin.py

--- News/news/admin.py
+++ News/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import News
 # Register your models here.
+
+
+class NewsAdmin(admin.ModelAdmin):
+    fields = ('created', 'title', 'text', 'link')
+
+
+admin.site.register(News, NewsAdmin)
