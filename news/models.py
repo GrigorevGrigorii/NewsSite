9a7dfe8418bfba00d499b04ad3bf30e2bbe86467
GrigorevGrigorii <st076639@student.spbu.ru>
2020-10-09 21:09:25 +0300

    Update

--- news/models.py
+++ news/models.py
@@ -7,4 +7,4 @@ class News(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=1048576)
     title = models.CharField(max_length=64)
-    link = models.IntegerField()
+    link = models.IntegerField(unique=True)
