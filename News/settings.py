2b4f8a00a9eac9708e1bbd4b7f9e4b9e6a40ec76
GrigorevGrigorii <st076639@student.spbu.ru>
2020-10-11 20:15:21 +0300

    Update

--- News/settings.py
+++ News/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'News.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_news_site',
-        'USER': 'grigorii',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
