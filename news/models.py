aa701c7ebfbab7157344baab4f7a4dddd7f465e4
Grigorev Grigorii <55593191+GrigorevGrigorii@users.noreply.github.com>
2020-10-16 11:00:01 +0300

    Update models.py

--- news/models.py
+++ news/models.py
@@ -10,6 +10,9 @@ class News(models.Model):
     title = models.CharField(max_length=256)
     link = models.IntegerField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.title
 
 
 class Comments(models.Model):
@@ -17,6 +20,9 @@ class Comments(models.Model):
     text = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     news = models.ForeignKey(News, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.text
 
 
 # class RepliesToComments(models.Model):
