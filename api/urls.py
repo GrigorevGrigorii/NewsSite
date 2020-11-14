fd74f4d988f39c14ad972b589797fe50b01712b6
GrigorevGrigorii <nigddd@gmail.com>
2020-11-14 12:17:23 +0300

    Update API

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import NewsAPI, CommentsAPI
+from .views import NewsAPI, CommentsAPI, UserAPI
 
 urlpatterns = [
     path('news/', NewsAPI.as_view()),
     path('comments/', CommentsAPI.as_view()),
+    path('user/', UserAPI.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,23 @@ from django.views import View
 
 from news.models import News, Comments
 
+import json
+
 # Create your views here.
+
+
+class UserAPI(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            data = {
+                'id': request.user.pk,
+                'username': request.user.username,
+                'email': request.user.email,
+            }
+            return JsonResponse(data, safe=False)
+
+        else:
+            return JsonResponse({'error': 'user is not authenticated'}, safe=False)
 
 
 class NewsAPI(View):
@@ -17,7 +33,7 @@ class NewsAPI(View):
             if specific_news is not None:
                 return JsonResponse(specific_news.as_dict(), safe=False)
             else:
-                return JsonResponse(False, safe=False, status=404)
+                return JsonResponse({'error': 'there is no such specific news'}, safe=False, status=404)
 
         elif q:
             q = q.rstrip('/')
@@ -29,27 +45,27 @@ class NewsAPI(View):
             return JsonResponse([news.as_dict() for news in all_news], safe=False)
 
     def post(self, request, *args, **kwargs):
-        title = request.GET.get('title')
-        text = request.GET.get('text')
-        link = 1
+        data = json.loads(request.body)
 
+        link = 1
         all_links = list(News.objects.values_list('link', flat=True).order_by('link'))
         if all_links and all_links[0] == 1:
             for existing_link in all_links:
                 if existing_link + 1 not in all_links:
                     link = existing_link + 1
                     break
+        data['link'] = link
 
         if request.user.is_authenticated:
-            new_news = News.objects.create(text=text, title=title, link=link, user=request.user)
-        else:
-            new_news = News.objects.create(text=text, title=title, link=link)
+            data['user'] = request.user
+        new_news = News.objects.create(**data)
 
         return JsonResponse(new_news.as_dict(), safe=False)
 
     def delete(self, request, *args, **kwargs):
-        link = request.GET.get('link')
-        specific_news = News.objects.filter(link=link).first()
+        data = json.loads(request.body)
+
+        specific_news = News.objects.filter(link=data['link']).first()
 
         if specific_news:
             specific_news.delete()
@@ -61,26 +77,30 @@ class CommentsAPI(View):
         news_link = int(request.GET.get('link'))
 
         specific_news = News.objects.filter(link=news_link).first()
+        if not specific_news:
+            return JsonResponse({'error': 'there is no such specific news'}, safe=False, status=404)
+
         all_comments = Comments.objects.filter(news=specific_news).order_by('-created')
         return JsonResponse([comment.as_dict() for comment in all_comments], safe=False)
 
     def post(self, request, *args, **kwargs):
-        text = request.GET.get('text')
-        news_link = request.GET.get('news_link')
+        data = json.loads(request.body)
 
-        specific_news = News.objects.filter(link=news_link).first()
+        specific_news = News.objects.filter(link=data['news_link']).first()
         if not specific_news:
-            return JsonResponse(False, safe=False, status=404)
+            return JsonResponse({'error': 'there is no such specific news'}, safe=False, status=404)
+
+        del data['news_link']
+        data['news'] = specific_news
 
         if request.user.is_authenticated:
-            comment = Comments.objects.create(text=text, news=specific_news, user=request.user)
-        else:
-            comment = Comments.objects.create(text=text, news=specific_news)
+            data['user'] = request.user
+        comment = Comments.objects.create(**data)
 
         return JsonResponse(comment.as_dict(), safe=False)
 
     def delete(self, request, *args, **kwargs):
-        comment_id = request.GET.get('id')
+        comment_id = json.loads(request.body)['id']
         comment = Comments.objects.filter(pk=comment_id)
 
         if comment:
